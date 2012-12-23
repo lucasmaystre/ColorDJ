8c56fac94fefc4ea9a8aff77bfd2b14604c33998
Lucas Maystre <lucas@maystre.ch>
2012-12-23 19:44:23 +0100

    Added last minute copyright & description info.

--- ColorDJ/Properties/AssemblyInfo.cs
+++ ColorDJ/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ColorDJ")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Mix the colors!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ColorDJ")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Baptiste Grund  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
